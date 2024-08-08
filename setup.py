08fbaccdbc393fe1ff85f5071e7f27959b9d4253
Thomas Wimmer <thomas.wimmer@ims.co.at>
2024-08-08 20:22:02 +0200

    Remove unrelated logging code from setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
 import setuptools
 
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
-#from distutils._log import log
-
 demo_cxx = setuptools.Extension(
     "demo._cxx", sources=[], libraries=["z"], language="c++"
 )
